c248fc0673070babfcb4d731ab3557f67814dd0b
秋葉惇平 <>
2021-06-03 22:59:55 +0900

    完成

--- src/assets/js/modules/header.js
+++ src/assets/js/modules/header.js
@@ -2,6 +2,7 @@ export function header(){
 
     const header = document.querySelector('.j-header');
     const header_btn = document.querySelector('.j-header_btn');
+    const header_link = document.querySelector('.j-header_link');
 
     header_btn.addEventListener('click',()=>{
         if(header.classList.contains('active')){
@@ -10,4 +11,20 @@ export function header(){
             header.classList.add('active');
         }
     });
+
+
+    $('a[href^="#"]').click(function(){
+        if(header.classList.contains('active')){
+            header.classList.remove('active');
+        }else{
+            header.classList.add('active');
+        }
+        var speed = 600;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+
 }
